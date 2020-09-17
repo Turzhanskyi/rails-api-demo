dc7ef4a472f225b1d44cbe1d963278feb47a3663
Vitalii <turzhansky81@gmail.com>
2020-09-17 18:19:48 +0300

    Add destroy Post with token_auth

--- api_client.rb
+++ api_client.rb
@@ -8,10 +8,9 @@ client = Faraday.new(url: 'http://localhost:3000') do |config|
   config.token_auth('62534b7d9402908319bd2cd443e83938')
 end
 
-response = client.post do |req|
-  req.url '/api/v1/posts'
+response = client.delete do |req|
+  req.url '/api/v1/posts/6'
   req.headers['Content-Type'] = 'application/json'
-  req.body = '{ "post": {"title": "Title", "body": "Text"} }'
 end
 
 puts Oj.load(response.body)
--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -5,7 +5,7 @@ module Api
     class PostsController < ApplicationController
       include ActionController::HttpAuthentication::Token::ControllerMethods
 
-      before_action :authenticate, only: %i[create]
+      before_action :authenticate, only: %i[create destroy]
 
       def index
         @posts = Post.order('created_at DESC')
@@ -19,6 +19,15 @@ module Api
           render json: @post, status: :created
         else
           render json: @post.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @post = @user.posts.find_by(params[:id])
+        if @post
+          @post.destroy
+        else
+          render json: { post: 'not found' }, status: :not_found
         end
       end
 
